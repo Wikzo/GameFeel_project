3925069b4093ef2fd91c6f6b71c02d3f537ac458
Wikzo <Linkthewindwaker@gmail.com>
2015-04-14 13:48:09 +0200

    Paper

---
+++ Physics course Unity/Assets/BallAddForce.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallAddForce : MonoBehaviour
+{
+
+    private Rigidbody _rigidbody;
+    public float Speed = 10;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Vector3 force;
+        Vector3 rotation;
+
+        force = Input.GetAxis("Horizontal")*Speed*Time.deltaTime * Vector3.forward;
+        //rotation = Input.GetAxis("Horizontal")*Speed*Time.deltaTime * Vector3.forward;
+
+
+        _rigidbody.AddTorque(force);
+    }
+}
---
+++ Physics course Unity/Assets/Scripts/AtForceAtPosition.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AtForceAtPosition : MonoBehaviour
+{
+    public Vector3 OffsetForWorldPos;
+    public GameObject UpForcePosition;
+
+    private Transform _upTransform;
+    private Rigidbody _rigidbody;
+
+    public Vector3 Force;
+
+    // Use this for initialization
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+
+        _upTransform = UpForcePosition.transform;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Vector3 force = -Physics.gravity * _rigidbody.mass;
+
+        force = Force;
+
+        // local position (correct)
+        _rigidbody.AddForceAtPosition(force, _upTransform.position);
+
+        // transform from local to world (correct)
+        //_rigidbody.AddForceAtPosition(force, transform.TransformPoint(new Vector3(1,1,0)));
+        
+        // world position (incorrect)
+        //_rigidbody.AddForceAtPosition(force, transform.position + OffsetForWorldPos);
+    }
+}
