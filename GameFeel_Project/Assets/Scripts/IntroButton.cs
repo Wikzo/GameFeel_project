c635f2019100b8aefde10be6871614e11302cf87
Wikzo <Linkthewindwaker@gmail.com>
2015-04-16 09:43:47 +0200

    Now works with new questionnaire + latin squares

--- GameFeel_Project/Assets/Scripts/IntroButton.cs
+++ GameFeel_Project/Assets/Scripts/IntroButton.cs
@@ -6,9 +6,13 @@ public class IntroButton : MonoBehaviour
     public void LoadLevel(string level)
     {
         if (level == "1")
+        {
+            Demographics.Instance.PostLatinSquare();
             Demographics.Instance.MyGameState = GameState.Playing;
+            Application.LoadLevel(level);
 
-        Application.LoadLevel(level);
+        }
+
     }
 
 }
