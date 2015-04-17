e51d61e36369ea2226a6d0ac53297549ffce25c8
Wikzo <Linkthewindwaker@gmail.com>
2015-04-17 23:37:50 +0200

    Now also ready for exe builds with same resolution as web

---
+++ GameFeel_Project/Assets/SetResolution.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetResolution : MonoBehaviour
+{
+#if UNITY_STANDALONE_WIN
+    // Use this for initialization
+    private void Awake()
+    {
+        PlayerPrefs.SetInt("Screenmanager Resolution Width", 960);
+        PlayerPrefs.SetInt("Screenmanager Resolution Height", 600);
+        PlayerPrefs.SetInt("Screenmanager Is Fullscreen mode", 0);
+
+        Screen.SetResolution(960, 600, false);
+
+        //DontDestroyOnLoad(gameObject);
+    }
+
+    void OnApplicationQuit()
+    {
+        PlayerPrefs.SetInt("Screenmanager Resolution Width", 960);
+        PlayerPrefs.SetInt("Screenmanager Resolution Height", 600);
+        PlayerPrefs.SetInt("Screenmanager Is Fullscreen mode", 0);
+    }
+
+    /*void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }*/
+
+#endif
+}
