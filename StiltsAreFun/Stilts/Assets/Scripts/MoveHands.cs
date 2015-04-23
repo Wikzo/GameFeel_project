05c7360edb626303c64dff157df6e2f79059e954
Wikzo <Linkthewindwaker@gmail.com>
2015-04-23 11:14:52 +0200

    Ultra hand

---
+++ StiltsAreFun/Stilts/Assets/Scripts/MoveHands.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveHands : MonoBehaviour
+{
+    public Transform LeftHand, RightHand;
+    public float MoveSpeed = 10;
+
+    // Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            LeftHand.Translate(LeftHand.forward * MoveSpeed * Time.deltaTime);
+            RightHand.Translate(RightHand.forward * -MoveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            LeftHand.Translate(LeftHand.forward * -MoveSpeed * Time.deltaTime);
+            RightHand.Translate(RightHand.forward * MoveSpeed * Time.deltaTime);
+        }
+    }
+}
