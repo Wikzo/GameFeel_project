fc32ed91fa4134cc85ed3b5f52d8e2275bd09f94
Wikzo <Linkthewindwaker@gmail.com>
2015-04-23 17:05:59 +0200

    Suction cup sorta working

---
+++ StiltsAreFun/Stilts/Assets/Scripts/HingeJointInformation.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HingeJointInformation : MonoBehaviour
+{
+    private HingeJoint _myHingeJoint;
+
+    public float Angle;
+
+    // Use this for initialization
+    private void Start()
+    {
+        foreach (HingeJoint hinge in GetComponents<HingeJoint>())
+        {
+            if (hinge.axis == Vector3.forward)
+            {
+                _myHingeJoint = hinge;
+                break;
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+       //Debug.Log(_myHingeJoint.angle);
+
+        Angle = _myHingeJoint.angle;
+
+
+        return;
+        if (Angle < 0)
+        {
+            JointLimits limits = _myHingeJoint.limits;
+
+            //limits.min = -90.5f;
+            limits.min = -91f;
+            limits.max = -89f;
+
+            _myHingeJoint.limits = limits;
+        }
+        else
+        {
+            JointLimits limits = _myHingeJoint.limits;
+
+            //limits.min = 90.5f;
+            limits.min = 89f;
+            limits.max = 91f;
+
+
+            _myHingeJoint.limits = limits;
+        }
+    }
+}
--- StiltsAreFun/Stilts/Assets/Scripts/MoveHands.cs
+++ StiltsAreFun/Stilts/Assets/Scripts/MoveHands.cs
@@ -3,28 +3,37 @@ using System.Collections;
 
 public class MoveHands : MonoBehaviour
 {
+    public Transform Root;
+    public Vector3 RotateAxis;
+    public float RotateSpeed = 10f;
+
     public Transform LeftHand, RightHand;
+    public Vector3 OpenCloseAxis;
     public float MoveSpeed = 10;
-
-    // Use this for initialization
-    private void Start()
-    {
-
-    }
 
     // Update is called once per frame
     private void Update()
     {
-
+        // move whole claw
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Root.Rotate(RotateAxis, RotateSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Root.Rotate(RotateAxis, -RotateSpeed * Time.deltaTime);
+        }
+        
+        // open/close claw
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            LeftHand.Translate(LeftHand.forward * MoveSpeed * Time.deltaTime);
-            RightHand.Translate(RightHand.forward * -MoveSpeed * Time.deltaTime);
+            LeftHand.Translate(OpenCloseAxis * MoveSpeed * Time.deltaTime);
+            RightHand.Translate(OpenCloseAxis * -MoveSpeed * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
-            LeftHand.Translate(LeftHand.forward * -MoveSpeed * Time.deltaTime);
-            RightHand.Translate(RightHand.forward * MoveSpeed * Time.deltaTime);
+            LeftHand.Translate(OpenCloseAxis * -MoveSpeed * Time.deltaTime);
+            RightHand.Translate(OpenCloseAxis * MoveSpeed * Time.deltaTime);
         }
     }
 }
